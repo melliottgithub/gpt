8513927dd9478246e1e05c6efb48926d071f5e1a
Mellio <kllmmc23@gmail.com>
2023-03-16 19:30:18 -0600

    Model update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -6,12 +6,12 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def generate_chat_gpt4_response(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="gpt-4-32k-0314",
         prompt=prompt,
         max_tokens=150,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.2,
     )
     return response.choices[0].text.strip()
 

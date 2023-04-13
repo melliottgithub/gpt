9b6de87d7f35e022142d7fc95d52fe2db223bae7
Mellio <kllmmc23@gmail.com>
2023-04-12 22:40:15 -0600

    refactor prompts

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,12 +5,9 @@ import openai
 # Set the OpenAI API key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Generate a response using the GPT-4 model
-
 
 def generate_chat_gpt4_response(prompt):
     try:
-        # Send a request to the OpenAI API
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=prompt,
@@ -19,79 +16,54 @@ def generate_chat_gpt4_response(prompt):
             stop=None,
             temperature=0.2,
         )
-        # Return the response text
         return response.choices[0].text.strip()
     except Exception as e:
-        # Log the error and re-raise the exception
         print(f"API request failed: {e}")
         raise e
 
-# Expand a short text into a more formal length with a friendly tone
-
 
 def expand_text(text):
-    prompt = f"Expand the following short text into a more formal length with a friendly tone: '{text}'"
-    expanded_text = generate_chat_gpt4_response(prompt)
-    return expanded_text
-
-# Fix grammar
+    prompt = f"Rewrite the following short text into a longer, more detailed version while maintaining a friendly tone:\n\nText: {text}\n"
+    return generate_chat_gpt4_response(prompt)
 
 
-def fix_spelling(text):
-    prompt = f"Fix grammar and indicate the changes made: '{text}'"
-    fixed_text = generate_chat_gpt4_response(prompt)
-    return fixed_text
-
-# Summarize a conversation in a concise and clear way
+def fix_grammar(text):
+    prompt = f"Correct any grammar errors in the text below and provide the revised version:\n\nOriginal text: {text}\n"
+    return generate_chat_gpt4_response(prompt)
 
 
 def summarize_conversation(text):
-    prompt = f"Summarize the following conversation in a concise and clear way: '{text}'"
-    summary = generate_chat_gpt4_response(prompt)
-    return summary
-
-# Perform sentiment analysis on a given text
+    prompt = f"Provide a clear and concise summary of the conversation below:\n\nConversation:\n{text}\n"
+    return generate_chat_gpt4_response(prompt)
 
 
 def analyze_sentiment(text):
-    prompt = f"Analyze the sentiment of the following text and classify it as positive, negative, or neutral: '{text}'"
-    sentiment = generate_chat_gpt4_response(prompt)
-    return sentiment
-
-# Classify a given text into a predefined category
+    prompt = f"Examine the sentiment of the following text and determine if it is positive, negative, or neutral:\n\nText: {text}\n"
+    return generate_chat_gpt4_response(prompt)
 
 
 def classify_text(text):
-    prompt = f"Classify the following text into a category: '{text}'"
-    category = generate_chat_gpt4_response(prompt)
-    return category
-
-# Extract keywords from a given text
+    prompt = f"Determine the most appropriate category for the following text:\n\nText: {text}\n"
+    return generate_chat_gpt4_response(prompt)
 
 
 def extract_keywords(text):
-    prompt = f"Extract the main keywords from the following text: '{text}'"
-    keywords = generate_chat_gpt4_response(prompt)
-    return keywords
-
-# Handle the Lambda function event
+    prompt = f"Identify the main keywords in the text below:\n\nText: {text}\n"
+    return generate_chat_gpt4_response(prompt)
 
 
 def lambda_handler(event, context):
     try:
-        # Parse the input parameters from the event body
         params = json.loads(event.get('body'))
         action = params.get('action')
         text = params.get('text')
 
-        # Check if the required parameters are present and not empty
         if not action or not text:
             return {
                 'statusCode': 400,
                 'body': json.dumps({'error': 'Missing parameters'})
             }
 
-        # Check if the action parameter is valid
         valid_actions = ['expand', 'fix', 'summarize',
                          'sentiment', 'classify', 'keywords']
         if action not in valid_actions:
@@ -100,27 +72,22 @@ def lambda_handler(event, context):
                 'body': json.dumps({'error': 'Invalid action'})
             }
 
-        # Call the appropriate function based on the action parameter
-        if action == 'expand':
-            response = expand_text(text)
-        elif action == 'fix':
-            response = fix_spelling(text)
-        elif action == 'summarize':
-            response = summarize_conversation(text)
-        elif action == 'sentiment':
-            response = analyze_sentiment(text)
-        elif action == 'classify':
-            response = classify_text(text)
-        elif action == 'keywords':
-            response = extract_keywords(text)
+        action_mapping = {
+            'expand': expand_text,
+            'fix': fix_grammar,
+            'summarize': summarize_conversation,
+            'sentiment': analyze_sentiment,
+            'classify': classify_text,
+            'keywords': extract_keywords
+        }
 
-        # Return the response in a JSON format
+        response = action_mapping[action](text)
+
         return {
             'statusCode': 200,
             'body': json.dumps({'response': response})
         }
     except Exception as e:
-        # Log the error and return a generic error message
         print(f"Error occurred: {e}")
         return {
             'statusCode': 500,
